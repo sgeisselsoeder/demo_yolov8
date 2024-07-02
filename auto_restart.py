1e7a2c15c884bf6fcf49df467ea913cf9011ea8d
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2024-07-02 14:36:02 +0200

    tries to maximize window automatically

--- auto_restart.py
+++ auto_restart.py
@@ -1,11 +1,21 @@
 from subprocess import Popen
 from time import sleep
+import pyautogui
+
 
 while True:
     print('Starting subprocess')
     p = Popen(['python3', 'main.py']) # something long running
-    time_to_run = 15
-    time_to_run = 10*60
+    sleep(12)
+    print('Maxmimizing window')
+    # pyautogui.hotkey('winleft', 'up')
+    # pyautogui.hotkey('alt', 'tab')
+    # pyautogui.hotkey('winleft', 'up')
+    pyautogui.keyDown('winleft')
+    pyautogui.press('up')
+    pyautogui.keyUp('winleft')
+    time_to_run = 17
+    # time_to_run = 10*60
     print("Sleeping for ", time_to_run, " seconds")
     sleep(time_to_run)
     # ... do other stuff while subprocess is running
